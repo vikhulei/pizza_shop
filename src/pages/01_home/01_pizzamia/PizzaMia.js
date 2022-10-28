86e9291b3577de3e9c334d8657bab6463da4a2fc
vikhulei <vikhulei@gmail.com>
2022-10-28 19:17:38 +0100

    pizza_mia component structure completed

--- src/pages/01_home/01_pizzamia/PizzaMia.js
+++ src/pages/01_home/01_pizzamia/PizzaMia.js
@@ -1,10 +1,48 @@
-import { Background, Wrapper } from "./PizzaMiaStyle"
+import { Background, Wrapper, CallToActionWrapper, PizzaMiaWrapper, Heading, SubHeading, OrderButton, IconsTextsWrapper, IconTextContainer,  IconWrapper, Icon, TextWrapper, IconHeading, IconSubHeading, PizzaImageContainer, PizzaImage } from "./PizzaMiaStyle"
+import pizza_mia1 from "../../../assets/home/pizza_mia1.png"
+import catIcon from "../../../assets/home/pizza_mia2.jpg"
+import cheeseIcon from "../../../assets/home/pizza_mia3.jpg"
 
 const PizzaMia = () => {
   return (
     <Background>
       <Wrapper>
-
+        <CallToActionWrapper>
+          <PizzaMiaWrapper>
+            <Heading>Pizza Mia</Heading>
+            <SubHeading>Your favourite pizza restaurant</SubHeading>
+            <OrderButton>
+              Order Now
+            </OrderButton>
+          </PizzaMiaWrapper>
+          <IconsTextsWrapper>
+            <IconTextContainer>
+              <IconWrapper>
+                <Icon src={catIcon} alt="cat icon" />
+              </IconWrapper>
+              <TextWrapper>
+                <IconHeading>Calories</IconHeading>
+                <IconSubHeading>
+                  Who's counting?
+                </IconSubHeading>
+              </TextWrapper>
+            </IconTextContainer>
+            <IconTextContainer>
+              <IconWrapper>
+                <Icon src={cheeseIcon} alt="cheese icon" />
+              </IconWrapper>
+              <TextWrapper>
+                <IconHeading>Cheese</IconHeading>
+                <IconSubHeading>
+                  Never enough
+                </IconSubHeading>
+              </TextWrapper>
+            </IconTextContainer>
+          </IconsTextsWrapper>
+        </CallToActionWrapper>
+        <PizzaImageContainer>
+          <PizzaImage src={pizza_mia1} alt="pizza mia" />
+        </PizzaImageContainer>
       </Wrapper>
     </Background>
   )
--- src/pages/01_home/01_pizzamia/PizzaMiaStyle.js
+++ src/pages/01_home/01_pizzamia/PizzaMiaStyle.js
@@ -6,14 +6,67 @@ const {background} = colors
 
 const Background = styled.div `
     background: url(${background});
-
 `
 
 const Wrapper = styled(WrapperStyled) `
-    height: 500px;
+    height: 700px;
     padding: 0 20px;
+
+`
+const CallToActionWrapper = styled.div `
+
 `
 
+const PizzaMiaWrapper = styled.div `
 
+`
 
-export { Background, Wrapper }
+const Heading = styled.p `
+
+`
+
+const SubHeading = styled.p `
+
+`
+
+const OrderButton = styled.a `
+
+`
+
+const IconsTextsWrapper = styled.div `
+
+`
+
+const IconTextContainer = styled.div `
+
+`
+
+const IconWrapper = styled.div `
+
+`
+
+const Icon = styled.img `
+
+`
+
+const TextWrapper = styled.div `
+
+`
+
+const IconHeading = styled.p `
+
+`
+
+const IconSubHeading = styled.p `
+
+`
+
+const PizzaImageContainer = styled.div `
+
+`
+
+const PizzaImage = styled.img `
+
+`
+
+export { Background, Wrapper, CallToActionWrapper, PizzaMiaWrapper, Heading, SubHeading, OrderButton, IconsTextsWrapper, IconTextContainer, IconWrapper, Icon, TextWrapper, IconHeading, IconSubHeading, PizzaImageContainer, PizzaImage }
