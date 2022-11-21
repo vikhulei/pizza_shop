8e1b1eb9a846e0bd7f58ea8bb88db883f416619f
vikhulei <vikhulei@gmail.com>
2022-11-21 20:36:31 +0000

    counter drafted, home structure completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css"
 import Navigation from "./components/navigation/Navigation"
 import PizzaMia from "./pages/01_home/01_pizzamia/PizzaMia";
 import ChooseSlice from "./pages/01_home/02_choose_slice/ChooseSlice";
+import SliceSlice from "./pages/01_home/03_slice_slice/SliceSlice";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Navigation />
       <PizzaMia />
       <ChooseSlice />
-      <PizzaMia />
+      <SliceSlice />
+      <ChooseSlice />
     </div>
   );
 }
---
+++ src/pages/01_home/03_slice_slice/SliceSliceStyle.js
@@ -0,0 +1,108 @@
+import styled from "styled-components"
+import { colors } from "../../../components/Colors"
+import { WrapperStyled } from "../../../components/ui/StyledComponents"
+
+const {background, dark} = colors
+
+const Background = styled.div `
+    background: url(${background});
+`
+
+const Wrapper = styled(WrapperStyled) `
+    height: 700px;
+    color: white;
+`
+
+const CallToActionWrapper = styled.div `
+    width: 50%;
+    height: 55%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+`
+
+const PizzaMiaWrapper = styled.div `
+
+`
+
+const Heading = styled.p `
+    font-family: Fuzzy Bubbles;
+    font-size: 6rem;
+    font-weight: bold;
+    color: ${dark};
+`
+
+const SubHeading = styled.p `
+    font-size: 1.2rem;
+    text-transform: uppercase;
+    margin-bottom: 30px;
+`
+
+const OrderButton = styled.a `
+    padding: 10px 35px;
+    background-color: ${dark};
+    border-radius: 15px;
+    border-top-left-radius: 0px;
+    color: black;
+    font-size: 0.9rem;
+    font-weight: bold;
+    cursor: pointer;
+    &:active {
+        background-color: darkred;
+    }
+`
+
+const IconsTextsWrapper = styled.div `
+    width: 80%;
+    display: flex;
+    justify-content: space-between;
+`
+
+const IconTextContainer = styled.div `
+    display: flex;
+`
+
+const IconWrapper = styled.div `
+
+`
+
+const Icon = styled.img `
+    height: 50px;
+`
+
+const TextWrapper = styled.div `
+    margin-left: 10px;
+    display: flex;
+    flex-direction: column;
+    font-size: 0.85rem;
+`
+
+const IconHeading = styled.p `
+    margin-bottom: 10px;
+    font-weight: bold;
+`
+    
+const IconSubHeading = styled.p `
+
+`
+
+const PizzaImageContainer = styled.div `
+    
+`
+
+const PizzaImage = styled.img `
+
+`
+
+const Counter = styled.div `
+    margin-top: 20px;
+    width: 200px;
+    height: 60px;
+    background-color: darkred;
+    color: white;
+    text-align: center;
+    line-height: 2;
+    font-size: 2rem;
+`
+
+export { Background, Wrapper, CallToActionWrapper, PizzaMiaWrapper, Heading, SubHeading, OrderButton, IconsTextsWrapper, IconTextContainer, IconWrapper, Icon, TextWrapper, IconHeading, IconSubHeading, PizzaImageContainer, PizzaImage, Counter }
