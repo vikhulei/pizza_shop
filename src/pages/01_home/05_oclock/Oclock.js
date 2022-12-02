8611962a507ba39b4d240250f7bb38895193d2cb
vikhulei <vikhulei@gmail.com>
2022-12-02 11:13:52 +0000

    pizza oclock animation drafted

--- src/pages/01_home/05_oclock/Oclock.js
+++ src/pages/01_home/05_oclock/Oclock.js
@@ -1,4 +1,5 @@
-import { Background, Wrapper } from "./OclockStyle"
+import { useState, useEffect, useRef, useMemo } from "react"
+import { Background, Wrapper, LeftSideWrapper, ColorHeading, Heading, SubHeading, QuoteBox, QuoteWrapper, QuoteContainer, Caption, AuthorWrapper, StarWrapper, DotsWrapper, Dot, Dot2, Dot3, Image } from "./OclockStyle"
 import clock1a from "../../../assets/home/clock1a.png"
 import clock1b from "../../../assets/home/clock1b.png"
 import clock1c from "../../../assets/home/clock1c.png"
@@ -6,13 +7,123 @@ import clock1c from "../../../assets/home/clock1c.png"
 
 const OClock = () => {
 
+  const quoteContainer = useRef()
+  const dotOne = useRef()
+  const dotTwo = useRef()
+  const dotThree = useRef()
+  
+  const test = (pixels) => {
+    // const toLeft = quoteContainer.current.getBoundingClientRect().x * -1 + 20
+    // quoteContainer.current.style.left =
+    // `${pixels}px`
+    // console.log(toLeft)
+    // const toLeft = quoteContainer.current.getBoundingClientRect().x * -1 + 20
+  }
+
+  const scrollToRef = (e) => {
+    const pixels = e.target.id
+    quoteContainer.current.style.animation = "none"
+    quoteContainer.current.style.left = 
+    `${pixels}px`
+
+    switch(pixels) {
+      case "0":
+        console.log(pixels)
+        dotOne.current.style.backgroundColor = "red"
+        dotTwo.current.style.backgroundColor = "yellow"
+        dotThree.current.style.backgroundColor = "yellow"
+        break
+      case "-1000":
+        dotOne.current.style.backgroundColor = "yellow"
+        dotTwo.current.style.backgroundColor = "red"
+        dotThree.current.style.backgroundColor = "yellow"
+        break
+      case "-2000":
+        dotOne.current.style.backgroundColor = "yellow"
+        dotTwo.current.style.backgroundColor = "yellow"
+        dotThree.current.style.backgroundColor = "red"
+        break
+    }
+  }
+
+
   return (
     <Background>
       <Wrapper>
-        
+        <LeftSideWrapper>
+          <ColorHeading onClick={test}>It's pizza o'clock</ColorHeading>
+          <Heading>We're Pizza</Heading>
+          <SubHeading>& we can make everyone happy</SubHeading>
+          <QuoteBox>
+            <QuoteContainer ref={quoteContainer}>
+          <QuoteWrapper className="quoteWrapper">
+            <Caption>PIZZA #1</Caption>
+            <Caption>Pizza is the only love triangle I want</Caption>
+          </QuoteWrapper>
+          <QuoteWrapper className="quoteWrapper">
+            <Caption>PIZZA #2</Caption>
+            <Caption>Pizza is the only love triangle I want</Caption>
+          </QuoteWrapper>
+          <QuoteWrapper className="quoteWrapper">
+            <Caption>PIZZA #3</Caption>
+            <Caption>Pizza is the only love triangle I want</Caption>
+          </QuoteWrapper>
+          </QuoteContainer>
+          </QuoteBox>
+          <DotsWrapper>
+            <Dot ref={dotOne}  id="0" onClick={scrollToRef}></Dot>
+            <Dot ref={dotTwo} id="-1000" onClick={scrollToRef}></Dot>
+            <Dot ref={dotThree} id="-2000" onClick={scrollToRef}></Dot>
+          </DotsWrapper>
+        </LeftSideWrapper>
       </Wrapper>
     </Background>
   )
 }
 
 export default OClock
+
+
+//   const [quoteOne, setQuoteOne] = useState("not yet")
+//   const [dotOne, setDotOne] = useState(true)
+//   const [test, setTest] = useState(false)
+
+//   const colorRef = useRef()
+//   const quoteOneRef = useRef()
+//   const quoteBox = useRef()
+
+//   const start = () => {
+//     setInterval(() => {
+//       // console.log("hello")
+//     }, 1000)
+//   }
+
+// const observer = useMemo(
+//   () => new IntersectionObserver(([entry]) => setDotOne(entry.isIntersecting)
+//     ),
+//     [],
+// )
+
+  // const trigger = () => {
+  //   const pizzaOne = quoteOneRef.current
+  //   const observer = new IntersectionObserver(entries => {
+  //     entries.forEach(entry => {
+  //       if(entry.isIntersecting) {
+  //           setQuoteOne("It's pizza o'clock")
+  //         } else {
+  //           setQuoteOne("not yet")
+  //           }
+  //         })
+  //       })
+        
+  //     }
+
+      // console.log(entry.boundingClientRect.x)
+  
+  // entry.isIntersecting ? console.log(entry.boundingClientRect.x) : console.log("false")
+
+    // It's pizza o'clock
+
+  // useEffect(() => {
+  //   observer.observe(quoteOneRef.current)
+  // }, [quoteOneRef, observer])
--- src/pages/01_home/05_oclock/OclockStyle.js
+++ src/pages/01_home/05_oclock/OclockStyle.js
@@ -1,8 +1,31 @@
-import styled from "styled-components"
+
+import styled, { keyframes, css } from "styled-components"
 import { colors } from "../../../components/Colors"
-import { ColorHeadingStyled, HeadingStyled, SubHeadingStyled, WrapperStyled } from "../../../components/ui/StyledComponents"
+import { CaptionStyled, ColorHeadingStyled, HeadingStyled, SubHeadingStyled, WrapperStyled } from "../../../components/ui/StyledComponents"
 
 const {background, dark} = colors
+
+const Animation = keyframes `
+    0% {left: 400px}
+    5% {left: 0px}
+    33% {left: 0px}
+    38% {left: -1000px}
+    64% {left: -1000px}
+    69% {left: -2000px}
+    99% {left: -2000px}
+    100% {left: -3000px}
+`
+
+// const Animation = keyframes `
+//     0% {transform: translateX(400px)}
+//     5% {transform: translateX(0px)}
+//     33% {transform: translateX(0px)}
+//     38% {transform: translateX(-1000px)}
+//     64% {transform: translateX(-1000px)}
+//     69% {transform: translateX(-2000px)}
+//     99% {transform: translateX(-2000px)}
+//     100% {transform: translateX(-3000px)}
+// `
 
 const Background = styled.div `
     background: url(${background});
@@ -28,14 +51,75 @@ const Heading = styled(HeadingStyled) `
 const SubHeading = styled(SubHeadingStyled) `
 
 `
+const QuoteBox = styled.div `
+    position: relative;
+    overflow: hidden;
+    width: 400px;
+    height: 70px;
+    white-space: nowrap;
+    display: flex;
+`
+const QuoteContainer = styled.div `
+    transition: 0.8s;
+    position: absolute;
+    width: fit-content;
+    display: flex;
+    animation-name: "";
+    animation-name: ${Animation};
+    animation-duration: 10s;
+    animation-iteration-count: infinite;
+`
 
 const QuoteWrapper = styled.div `
+    min-width: 1000px;
+    transition: 0.5s;
+    `
+
+const Caption = styled(CaptionStyled) `
 
 `
+
+const AuthorWrapper = styled.div `
+
+`
+
+const StarWrapper = styled.div `
+
+`
+
+const DotsWrapper = styled.div `
+    cursor: pointer;
+    margin-top: 30px;
+    display: flex;
+    width: 50px;
+    justify-content: space-between;
+`
+
+const Dot = styled.div `
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: yellow;
+`
+
+const Dot2 = styled.div `
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: red;
+`
+
+const Dot3 = styled.div `
+    width: 10px;
+    height: 10px;
+    border-radius: 50%;
+    background-color: red;
+`
+
 
 const Image = styled.img `
 
 `
 
 
-export { Background, Wrapper }
+export { Background, Wrapper, LeftSideWrapper, ColorHeading, Heading, SubHeading, QuoteBox, QuoteContainer, QuoteWrapper, Caption, AuthorWrapper, StarWrapper, DotsWrapper, Dot, Dot2, Dot3, Image }
