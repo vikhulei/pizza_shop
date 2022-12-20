d33272753cbbbca91eff01738e864f9ecec981f9
vikhulei <vikhulei@gmail.com>
2022-12-20 18:48:29 +0000

    pizza mia section mobile and tablet styles

--- src/pages/01_home/01_pizzamia/PizzaMiaStyle.js
+++ src/pages/01_home/01_pizzamia/PizzaMiaStyle.js
@@ -1,6 +1,6 @@
 import styled from "styled-components"
 import { colors } from "../../../components/Colors"
-import { WrapperStyled } from "../../../components/ui/StyledComponents"
+import { WrapperStyled, CaptionStyled, CaptionBoldStyled } from "../../../components/ui/StyledComponents"
 
 const {background, dark} = colors
 
@@ -11,8 +11,17 @@ const Background = styled.div `
 `
 
 const Wrapper = styled(WrapperStyled) `
-    height: 700px;
+    max-height: 700px;
+    min-height: 500px;
+    height: 50vmax;
     color: white;
+    @media screen and (max-width: 800px) {
+        flex-direction: column-reverse;
+        max-height: 2000px;
+        height: auto;
+        padding: 80px 0;
+        // background-color: red;
+    }
 `
 
 const CallToActionWrapper = styled.div `
@@ -21,6 +30,11 @@ const CallToActionWrapper = styled.div `
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    @media screen and (max-width: 800px) {
+        text-align: center;
+        width: 100%;
+        // background-color: red;
+    }
 `
 
 const PizzaMiaWrapper = styled.div `
@@ -29,11 +43,10 @@ const PizzaMiaWrapper = styled.div `
 
 const Heading = styled.p `
     font-family: Fuzzy Bubbles;
-    font-size: 6vw;
-    font-size: 4rem;
-    font-size: min(max(4rem, 6vw), 6rem);
+    font-size: min(max(4.5rem, 8.5vw), 7rem);
     font-weight: bold;
     color: ${dark};
+    width: 100%;
 `
 
 const SubHeading = styled.p `
@@ -60,10 +73,22 @@ const IconsTextsWrapper = styled.div `
     width: 80%;
     display: flex;
     justify-content: space-between;
+    @media screen and (max-width: 800px) {
+        flex-direction: column;
+        gap: 50px;
+        width: 100%;
+        align-items: center;
+        margin-top: 100px;
+    }
 `
 
 const IconTextContainer = styled.div `
     display: flex;
+    @media screen and (max-width: 800px) {
+        flex-direction: column;
+        justify-content: space-between;
+        width: 30vw;
+    }
 `
 
 const IconWrapper = styled.div `
@@ -72,6 +97,10 @@ const IconWrapper = styled.div `
 
 const Icon = styled.img `
     height: 50px;
+    @media screen and (max-width: 800px) {
+        height: auto;
+        width: 100%;
+    }
 `
 
 const TextWrapper = styled.div `
@@ -79,19 +108,30 @@ const TextWrapper = styled.div `
     display: flex;
     flex-direction: column;
     font-size: 0.85rem;
+    @media screen and (max-width: 800px) {
+        margin-top: 20px;
+        height: 50px;
+        justify-content: space-between;
+    }
 `
 
-const IconHeading = styled.p `
+const IconHeading = styled(CaptionBoldStyled) `
     margin-bottom: 10px;
     font-weight: bold;
 `
     
-const IconSubHeading = styled.p `
-
+const IconSubHeading = styled(CaptionStyled) `
+font-size: 0.9rem;
 `
 
 const PizzaImageContainer = styled.div `
+
     max-width: 40vw;
+    @media screen and (max-width: 800px) {
+        max-width: 100%;
+        width: 100%;
+        padding: 50px;        
+    }
 `
 
 const PizzaImage = styled.img `
