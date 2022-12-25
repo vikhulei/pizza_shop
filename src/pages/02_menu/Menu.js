304ddfe38a58b082edf2cd3c2221d5fa96861cd0
vikhulei <vikhulei@gmail.com>
2022-12-25 18:39:07 +0000

    menu page added

--- src/pages/02_menu/Menu.js
+++ src/pages/02_menu/Menu.js
@@ -1,10 +1,16 @@
 import Menu from "../01_home/04_menu/Menu";
+import FooterWide from "../../components/footerWide/FooterWide";
+import MinutesAway from "../01_home/06_minutesAway/MinutesAway";
+import FooterNarrow from "../../components/footerNarrow/FooterNarrow";
 import { ComponentsWrapper } from "../../components/ui/Wrappers";
 
 const MenuComponent = () => {
   return (
     <ComponentsWrapper>
         <Menu />
+        <FooterWide />
+        <MinutesAway />
+        <FooterNarrow />
     </ComponentsWrapper>
   )
 }
